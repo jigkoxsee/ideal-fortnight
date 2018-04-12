b4cb23fe8539ab9e463e49041b2f37009ba11548
Supakorn Warodom <supakorn@satang.com>
2018-04-12 21:25:35 +0700

    Can display transaction from specific address and can filter unspent utxo

--- src/zcoin_wallet/views/addr_transactions.cljs
+++ src/zcoin_wallet/views/addr_transactions.cljs
@@ -1,6 +1,29 @@
 (ns zcoin-wallet.views.addr-transactions
   (:require [zcoin-wallet.handlers.get-addr-transactions :as h]))
 
+
+(defn field [label value]
+  [:div.row
+   [:div.col-md-2 label]
+   [:div.col-md-6 value]])
+
+;; TODO: Use schema to validate data
+;; TODO: show source address
+(defn transaction [{:keys [txid value]}]
+  [:div.row
+    [field "TxID" txid]
+    [field "Value" value]])
+
+
 (defn page []
-  [:div
-   [:h2 "Transactions"]])
+  (let [txs (h/get-transactions "a95vY5GUY7piVhe9iuPYYd2pysbxJMmJmY")]
+    [:div
+     [:h2 "Transactions"]
+     [:div
+      (for [tx txs]
+        [transaction tx])]]))
+
+(comment
+  (h/get-transactions "a95vY5GUY7piVhe9iuPYYd2pysbxJMmJmY")
+
+  (h/get-transactions ""))
